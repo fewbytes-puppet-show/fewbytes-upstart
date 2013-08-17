1871f60af028d2a4a69ebb5cc7e19de0a87705e7
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-08-17 14:48:12 +0300

    Add start_on and stop_on parameters

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,18 +1,22 @@
 define upstart::service(
 	$service_name=$title,
 	$ensure=running,
+	$enable=true,
 	$user=undef,
 	$group=undef,
 	$chdir=undef,
 	$exec=undef,
 	$script=undef,
 	$type=foreground,
+	$start_on="runlevel [2345]",
+	$stop_on="runlevel [^2345]",
 	$template="upstart/init.conf.erb"
 ) {
 	include upstart::params
 	include upstart::utils
-	
+
 	file{"${upstart::params::svc_dir}/${service_name}.conf":
+		ensure => $enable ? { true => present, false => absent, default => present},
 		content => template($template),
 		mode => 644
 	}
