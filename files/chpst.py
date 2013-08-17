735d3859ae4d0cb1434e972ccdef3cca004ff746
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-08-17 17:15:59 +0300

    Fixed python 2.6 compat

--- files/chpst.py
+++ files/chpst.py
@@ -1,14 +1,12 @@
 #! /usr/bin/env python
 
-import argparse, sys, os
+import optparse, sys, os
 import pwd, grp
 
-parser = argparse.ArgumentParser("chpst.py")
-parser.add_argument("-u", default=None, dest="user", help="user to run command as")
-parser.add_argument("-g", default=None, dest="group", help="group to run command as")
-parser.add_argument("executable", nargs=1, help="Command executable")
-parser.add_argument('command_args', nargs='*', help="Command arguments")
-opts = parser.parse_args()
+parser = optparse.OptionParser()
+parser.add_option("-u", default=None, dest="user", help="user to run command as")
+parser.add_option("-g", default=None, dest="group", help="group to run command as")
+(opts, args) = parser.parse_args()
 
 if opts.group:
 	gid = grp.getgrnam(opts.group).gr_gid
@@ -18,4 +16,4 @@ if opts.user:
 	uid = pwd.getpwnam(opts.user).pw_uid
 	os.setresuid(uid, uid, uid)
 
-os.execvp(opts.executable[0], opts.command_args)
+os.execvp(args[0], args)
