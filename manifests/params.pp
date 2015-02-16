54fe6d1a01f5c4b3cd9c2efa627b0c380581bf9b
ronen <narkisr@gmail.com>
2015-02-16 18:42:04 +0200

    fixing lint issues

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,25 +1,26 @@
+# upstart default params
 class upstart::params {
-	$svc_dir = "/etc/init"
-	case $::operatingsystem {
-		RedHat, CentOs: {
-			if versioncmp($::operatingsystemrelease, "6.0") < 0 {
-				fail("This platform/version combo is not supported for upstart")
-			} else {
-				$setuid_supported = false
-				$setgid_supported = false
-			}
-		}
-		Ubuntu: {
-			if versioncmp($::operatingsystemrelease, "12.04") < 0 {
-				$setuid_supported = false
-				$setgid_supported = false				
-			} else {
-				$setuid_supported = true
-				$setgid_supported = true
-			}
-		}
-		default: {
-			fail("This platform/version combo is not supported for upstart")
-		}
-	}
+  $svc_dir = '/etc/init'
+  case $::operatingsystem {
+    RedHat, CentOs: {
+      if versioncmp($::operatingsystemrelease, '6.0') < 0 {
+        fail('This platform/version combo is not supported for upstart')
+        } else {
+          $setuid_supported = false
+          $setgid_supported = false
+        }
+    }
+    Ubuntu: {
+      if versioncmp($::operatingsystemrelease, '12.04') < 0 {
+        $setuid_supported = false
+        $setgid_supported = false
+        } else {
+          $setuid_supported = true
+          $setgid_supported = true
+        }
+    }
+    default: {
+      fail('This platform/version combo is not supported for upstart')
+    }
+  }
 }
